e0efe9e0053b7907de6615af912351a7375505ae
antti-riiko <riikonen.j.antti@gmail.com>
2024-09-10 20:06:10 +0300

    done

---
+++ src/components/Setup2FA.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./ui/button";
+
+const Setup2FA = (props: { qrCodeUrl: string; switchForm: () => void }) => {
+  const { qrCodeUrl, switchForm } = props;
+  return (
+    <div className="p-4">
+      <img src={qrCodeUrl} alt="Auth QR Code" />
+      <Button onClick={switchForm}>Press here when QR code is scanned</Button>
+    </div>
+  );
+};
+
+export default Setup2FA;
